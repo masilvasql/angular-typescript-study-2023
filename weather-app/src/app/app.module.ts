4f5240a0edaa85528fa26ed0acfa53078b8c0674
marcelo.silva <marcelo.silva@neomind.com.br>
2023-08-20 20:40:48 -0300

    weather-app

---
+++ weather-app/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
+import { WeatherCardComponent } from './modules/weather/components/weather-card/weather-card.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherHomeComponent,
+    WeatherCardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule,
+    MatSlideToggleModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ weather-app/src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = '3dfb51e36a15f79cea06f4d38b67a90f';
+
+
+  constructor(private http:HttpClient ) { }
+
+  getWeatherDatas(cityName:string):Observable<any>{
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,{})
+  }
+}
