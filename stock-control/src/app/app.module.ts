2b2849a29fb0f5c99e3ceca723d3abc80e2dcd4e
marcelo.silva <marcelo.silva@neomind.com.br>
2023-08-25 21:08:52 -0300

    Iniciando com forms reativos

---
+++ stock-control/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import {CardModule} from 'primeng/card';
+import {ToastModule} from 'primeng/toast'
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './modules/home/home.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    // PrimeNG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ stock-control/src/app/modules/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  loginCard = true;
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  signUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do formulário de login: ', this.loginForm.value);
+  }
+
+  onSubmitSignUpForm(): void {
+    console.log('Dados do formulário de cadastro: ', this.signUpForm.value);
+  }
+}
