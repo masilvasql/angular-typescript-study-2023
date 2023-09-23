19de55d4ebf7a551372cd5cac95b02aa7691a1df
marcelo.silva <marcelo.silva@neomind.com.br>
2023-09-23 14:56:09 -0300

    continuação do projeto.

---
+++ stock-control/src/app/models/interfaces/products/response/DeleteProductResponse.ts
@@ -0,0 +1,8 @@
+export interface DeleteProductResponse{
+  id: string;
+  name: string;
+  amount: number;
+  price: number;
+  description: string;
+  category_id: string;
+}
--- stock-control/src/app/modules/products/page/products-home/products-home.component.ts
+++ stock-control/src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Subject, takeUntil } from 'rxjs';
+import { DeleteProductAction } from 'src/app/models/interfaces/products/event/DeleteProductAction';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
@@ -23,7 +25,8 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
     private productsService: ProductsService,
     private productsDataTransferService: ProductsDataTransferService,
     private router: Router,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private confirmationService:ConfirmationService
   ) { }
   ngOnInit(): void {
     this.getServiceProductsDatas();
@@ -63,6 +66,59 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
           this.router.navigate(['/dashboard']);
         }
       })
+  }
+
+  handleProductAction(event:EventAction): void {
+    console.log(event);
+  }
+
+  handleDeleteProductAction(event:DeleteProductAction){
+    this.confirmationService.confirm({
+      message: `Deseja realmente excluir o produto ${event.product_name}?`,
+      header: 'Confirmação de Exclusão',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'pi pi-check',
+      rejectIcon: 'pi pi-times',
+      acceptButtonStyleClass: 'p-button-success',
+      rejectButtonStyleClass: 'p-button-danger',
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      accept: () => {
+        this.deleteProduct(event.product_id);
+      },
+      reject: () => {
+      }
+    })
+  }
+  deleteProduct(product_id: string) {
+    if(product_id){
+      let response = this.productsService
+      .deleteProduct(product_id)
+      .pipe(
+        takeUntil(this._destroyed$)
+      ).subscribe({
+        next: (response) => {
+          if(response){
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Produto excluído com sucesso',
+              life: 3000
+            });
+            this.getProductsDatas();
+          }
+        },
+        error: (error) => {
+          console.log(error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao excluir o produto',
+            life: 3000
+          });
+        }
+      })
+    }
   }
 
   ngOnDestroy(): void {
